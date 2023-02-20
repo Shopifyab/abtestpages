81de3c9734e58049ef3641b1ddc9abfc9efa59d4
Shopifyab <70981808+Shopifyab@users.noreply.github.com>
2023-02-20 14:42:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,23 @@ if (currentUrl.includes("example.com")) {
 } else {
   console.log("Current URL does not contain 'example.com'");
 }
-var abtd = localStorage.getItem("ABTL");
+
+const scriptFunction = async () => {
+  var abtd = localStorage.getItem("ABTL");
 // var abti = sessionStorage.getItem("abti");
 
 // use abtd and abti in your A/B testing code
 console.log("abtd", abtd);
+const result = await fetch(`https://elevateab.app/api/scripttag`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				abtd: abtd,
+			}),
+		});
+  console.log("the result", result)
+}
+
+scriptFunction()
