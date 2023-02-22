d5f71bc4ea23a71b0e31ad3ab7fc31b534902885
Shopifyab <70981808+Shopifyab@users.noreply.github.com>
2023-02-22 18:50:41 -0500

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,26 @@
+console.log("the file")
+var currentUrl = window.location.href;
+if (currentUrl.includes("example.com")) {
+  console.log("Current URL contains 'example.com'");
+} else {
+  console.log("Current URL does not contain 'example.com'");
+}
+
+const scriptFunction = async () => {
+  var abtd = localStorage.getItem("ABTL");
+
+// use abtd and abti in your A/B testing code
+console.log("abtd", abtd);
+const result = await fetch(`${window.Shopify.shop}/scripttag`, {
+			method: "POST", 
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				abtd: abtd,
+			}),
+		});
+  console.log("the result", result)
+}
+
+scriptFunction()
