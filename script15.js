98e501e36c36646713fc5de686238985d1555c9b
Shopifyab <70981808+Shopifyab@users.noreply.github.com>
2023-03-04 20:31:33 -0500

    Update and rename script14.js to script15.js

---
+++ script15.js
@@ -0,0 +1,111 @@
+const runningTests = elevateTests;
+
+const runFunction = () => {
+  let testList = JSON.parse(localStorage.getItem("ABTL")) || {};
+  let collectionsList = JSON.parse(sessionStorage.getItem("ABCL")) || {};
+  let addedViews = JSON.parse(sessionStorage.getItem("ABAV")) || {};
+  let addedUniqueViews = JSON.parse(localStorage.getItem("ABAU")) || {};
+
+  function getParams(url) {
+    const parser = new URL(url);
+    return parser.search.slice(1);
+  }
+
+  const url = new URL(window.location.href);
+  const [hrefUrl] = url.href.split("?");
+  const paramsLine = getParams(window.location.href) || "";
+
+  const handleTest = async ({ testID, unique }) => {
+    const newObj = testList || {};
+    const testVar = unique
+      ? Object.keys(runningTests[testID])[
+          Math.floor(Math.random() * Object.keys(runningTests[testID]).length)
+        ]
+      : newObj[testID];
+
+    const seenThroughCollection = !!collectionsList?.[testID];
+
+    if (!addedUniqueViews[testID]) {
+      firstView = true;
+      addedUniqueViews[testID] = true;
+      console.log("Set Unique Views: ", addedUniqueViews);
+      localStorage.setItem("ABAU", JSON.stringify(addedUniqueViews));
+    }
+
+    if (!addedViews[testID]) {
+      if (seenThroughCollection) {
+        collectionsList[testID] = "added";
+        console.log("Set Collections List: ", collectionsList);
+        sessionStorage.setItem("ABCL", JSON.stringify(collectionsList));
+      }
+      addedViews[testID] = true;
+
+      console.log("Set Added View: ", addedViews);
+      sessionStorage.setItem("ABAV", JSON.stringify(addedViews));
+      console.log("Send Add View Request");
+      const addViewUrl = `https://${storeDomain}/apps/elevateab/addview`;
+      const params = new URLSearchParams({
+        testID,
+        Variation: testVar,
+        unique: firstView,
+        clickthrough: seenThroughCollection,
+        referrer: document.referrer,
+        store,
+      });
+
+      fetch(addViewUrl, {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: params.toString(),
+      });
+    }
+
+    const redirectURL = runningTests[testID][testVar] + "?" + paramsLine;
+    console.log("Redirecting url: ", redirectURL);
+  };
+
+  const handleDecideTests = async (tests) => {
+    let newObj = testList || {};
+
+    for (let i = 0; i < tests.length; i++) {
+      const keys = Object.keys(runningTests[tests[i]]);
+      testVar = keys[Math.floor(Math.random() * keys.length)];
+      newObj[tests[i]] = testVar;
+    }
+    console.log("Set Test List in HandleDecideTests: ", newObj);
+     localStorage.setItem("ABTL", JSON.stringify(newObj));
+  };
+
+  let onStart = () => {
+    let missingTests = [];
+    for (let item in runningTests) {
+      if (!testList?.[item]) {
+        missingTests.push(item);
+      }
+    }
+    if (missingTests.length > 0) {
+      handleDecideTests(missingTests);
+    }
+
+    const matchingUrl = Object.values(runningTests).find((urls) =>
+      Object.values(urls).includes(hrefUrl)
+    );
+    if (matchingUrl) {
+      const testID = Object.keys(runningTests).find(
+        (key) => runningTests[key] === matchingUrl
+      );
+
+  if (testID && !addedViews?.[testID]) {
+    const unique = !testList || !testList[testID];
+    handleTest({ testID, unique });
+  }
+
+}
+
+  }
+  if (paramsLine != "control") {
+    onStart()
+  }
+
+}
+runFunction()
